96c537b1f288cd29dbefe38645e961de1fdef4c2
smarunich <marunich.s@gmail.com>
2019-01-23 04:15:26 +0000

    fixing ansible version to 2.6.12 from RHEL 7.6 default 2.7

--- provisioning/provision_jumpbox.sh
+++ provisioning/provision_jumpbox.sh
@@ -7,6 +7,7 @@ systemctl enable squid
 systemctl start squid
 systemctl enable nginx
 systemctl start nginx
+pip install --upgrade ansible==2.6.12
 pip install --upgrade avisdk 
 ansible-galaxy install avinetworks.avisdk avinetworks.aviconfig --force
 git clone git://github.com/ansible/ansible-runner /tmp/ansible-runner
